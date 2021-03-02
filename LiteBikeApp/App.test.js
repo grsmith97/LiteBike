7d1bf4d9811e262cd3ef729fe55d55a45c0a40b0
Glenn Smith <grsmith97@gmail.com>
2021-03-02 13:35:26 -0600

    - Put more information in ReadMe - Added snapshot test - Fix typo in readme - Added newline to end of files that don't have them - Add missing semicolon - Remove item from jest rule

--- LiteBikeApp/App.test.js
+++ LiteBikeApp/App.test.js
@@ -11,6 +11,10 @@ describe('Given App component', () => {
       component = renderer.create(<App />);
     });
 
+    it('Then the component matches the snapshot', () => {
+      expect(component.toJSON()).toMatchSnapshot();
+    });
+
     it('Then Hello World appears on the screen', () => {
       const text = component.root.findByProps({'data-testing': 'hello-world'});
       expect(text.props.children).toBe('Hello, world!');
